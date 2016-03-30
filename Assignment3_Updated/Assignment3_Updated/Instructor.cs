2e2c53d3acf46e8764e947c6d87aaa443e7430be
Owaldi <S00964551@acad.tri-c.edu>
2016-03-29 21:09:50 -0400

    Added Assignment 3

---
+++ Assignment3_Updated/Assignment3_Updated/Instructor.cs
@@ -0,0 +1,24 @@
+﻿namespace Assignment3_Updated
+{
+    class Instructor
+    {
+        public string Name;
+        public string CourseName;
+
+        public Instructor(string name, string coursename)
+        {
+            this.Name = name;
+            this.CourseName = coursename;
+        }
+
+        public void SetStudentGrade(Student student, int grade)
+        {
+            student.SetGrade(grade);
+        }
+
+        public void Print()
+        {
+            System.Console.WriteLine(this.Name);
+        }
+    }
+}
---
+++ Assignment3_Updated/Assignment3_Updated/Student.cs
@@ -0,0 +1,27 @@
+﻿namespace Assignment3_Updated
+{
+    class Student
+    {
+        public string Name;
+        public int Grade;
+        public Instructor Teacher;
+
+        public Student(string s, Instructor i)
+        {
+            this.Name = s;
+            this.Teacher = i;
+            this.Grade = 0;
+        }
+
+        public void SetGrade(int grade)
+        {
+            this.Grade = grade;
+        }
+
+        public void PrintNameGradeAndTeacher()
+        {
+            System.Console.WriteLine(this.Name + this.Grade);
+            this.Teacher.Print();
+        }
+    }
+}
